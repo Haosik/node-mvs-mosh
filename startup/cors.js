6164fc1567f5ca7f95cde2611e1a8d37a0a49646
Haosik <noobbaster@gmail.com>
2019-01-14 00:27:44 +0200

    auth and admin works. 183 dn

--- startup/cors.js
+++ startup/cors.js
@@ -1,5 +1,9 @@
-const cors = require("cors");
+const cors = require('cors');
+
+const corsOptions = {
+  exposedHeaders: 'x-auth-token'
+};
 
 module.exports = function(app) {
-  app.use(cors());
+  app.use(cors(corsOptions));
 };
